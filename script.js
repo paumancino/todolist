dc6252e5e1e8a1f67d52bf3c174f21fdef309b9d
AgustinMassa <amassa@sinergiasoluciones.com.ar>
2022-09-09 12:59:25 -0300

    Termine el proyecto

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function agregarItemLista(nombre, key) {
   label.setAttribute("class", "toggler-wrapper style-8");
   div2.setAttribute("class", "toggler-slider");
   div3.setAttribute("class", "toggler-knob");
+  li.setAttribute("key", "item--" + key);
   checkbox.addEventListener("change", toggleTareaHecha);
   const p = document.createElement("p");
   p.innerText = nombre;
@@ -40,7 +41,7 @@ function agregarItemLista(nombre, key) {
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
   const dataForm = Object.fromEntries(new FormData(evento.target));
-  if (!dataForm.nombre) return alert("No dejes las cosas vacias");
+  if (!dataForm.nombre) return alert("❤ No agregaste ninguna tarea ❤");
 
   const key = guardarItemStorage(dataForm.nombre);
   agregarItemLista(dataForm.nombre, key);
@@ -50,5 +51,20 @@ form.addEventListener("submit", (evento) => {
 function toggleTareaHecha() {
   const key = this.getAttribute("key").slice(6);
   const p = document.querySelector(`[key=tarea-${key}]`);
+  const li = document.querySelector(`[key=item--${key}]`);
   p.classList.toggle("tachado");
+  li.classList.toggle("hecho");
 }
+
+const button = document.querySelector("#btn-eliminar-tareas");
+
+button.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  console.log("hice click");
+  const tareasHechas = document.querySelectorAll(".hecho");
+  console.log(tareasHechas);
+  for (let index = 0; index < tareasHechas.length; index++) {
+    tareasHechas[index].remove();
+  }
+})
+
